ae763736e21e1aad7a714c9ec4d1c784a16940a5
Yassine Rakibi <yassine@arcraven.com>
2023-09-20 22:24:35 +0200

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        cls_objs = {}
+        for key, obj in FileStorage.__objects.items():
+            if isinstance(obj, cls):
+                cls_objs[key] = obj
+        return cls_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +30,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        if obj:
+            for needle, data in FileStorage.__objects.items():
+                if obj == data:
+                    del FileStorage.__objects[needle]
+                    break
 
     def reload(self):
         """Loads storage dictionary from file"""
