8ff60defdec3a2e3bb1ef0cebce1e622a1b4042e
Yassine Rakibi <yassine@arcraven.com>
2023-09-21 00:26:21 +0200

    amenity + place

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+
+envDB = os.environ.get("HBNB_TYPE_STORAGE")
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    if envDB != "db":
+        name = ""
+    else:
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place", secondary=place_amenity, back_populates="amenities"
+        )
