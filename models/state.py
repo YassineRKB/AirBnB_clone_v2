02c75c395d73e10782388a9a2c08d3a3f0b90559
Yassine Rakibi <yassine@arcraven.com>
2023-09-20 22:46:42 +0200

    task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+variable_name = 'HBNB_TYPE_STORAGE'
+env_value = os.environ.get(variable_name)
+Base = declarative_base()
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if env_value != 'db':
+        cities = self.link()
+    else:
+        cities = relationship('City', foreign_keys='City.state_id')
+
+    def link(self):
+        from models.__init__ import storage
+        obj_list = []
+        strg = storage.all(City)
+        for value in strg:
+            if self.id == value.state_id:
+                obj_list.append(
+                    value
+                )
+        return obj_list
