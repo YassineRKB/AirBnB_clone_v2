366082d737861ac411c9231957e25ac7f784f659
Yassine Rakibi <yassine@arcraven.com>
2023-09-21 00:10:10 +0200

    task 6

--- models/user.py
+++ models/user.py
@@ -12,15 +12,9 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = "users"
-    if envDB != "db":
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-    else:
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user")
+    reviews = relationship("Review", backref="user")
