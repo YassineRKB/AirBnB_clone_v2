f4d69399529f87f8eb98058d4e073a25a6746bf7
YassineRKB <yassine@arcraven.com>
2023-10-05 22:00:24 +0200

    airbnb - deploy static: task 101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,58 @@
+# Puppet manifest for setting up web servers for web_static deployment
+# Update package repositories
+exec { 'apt-update':
+  command => 'sudo apt update -y',
+  path    => '/usr/bin',
+}
+
+# Install nginx package
+-> package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-update'],
+}
+
+# Define variables and configurations
+$alx_base        = '/data/web_static'
+$alx_tests       = "${alx_base}/releases/test"
+$alx_shared      = "${alx_base}/shared"
+$alx_index       = "${alx_tests}/index.html"
+$alx_current     = "${alx_base}/current"
+$alx_nginx_sites  = '/etc/nginx/sites-available/default'
+
+# Create directories and set ownership
+-> file { [$alx_tests, $alx_shared]:
+  ensure => 'directory',
+}
+
+-> file { $alx_index:
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+
+-> file { $alx_current:
+  ensure => 'link',
+  target => $alx_tests,
+  force  => true,
+}
+
+-> exec { 'chown-web-static':
+  command => "sudo chown -R ubuntu:ubuntu ${alx_base}",
+  path    => '/usr/bin',
+  require => File[$alx_tests],
+}
+
+# Configure nginx
+$nginx_config = 'location /hbnb_static/ { alias $alx_current/; }'
+-> file { $alx_nginx_sites:
+  ensure  => 'file',
+  content => template('path/to/nginx_template.erb'), # You can use an ERB template for this configuration
+  require => Exec['chown-web-static'],
+}
+
+# Restart nginx service
+-> service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  require   => File[$alx_nginx_sites],
+  subscribe => Exec['chown-web-static'],
+}
