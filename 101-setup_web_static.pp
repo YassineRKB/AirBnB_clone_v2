f7de92ac79bb2b296fccae6c0dfe63ac8e106d45
Yassine Rakibi <yassine@arcraven.com>
2023-10-09 03:41:45 +0200

    task 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,28 +1,75 @@
-exec { 'aptoperations':
-  command => 'apt update && apt -y install nginx',
-  provider => shell,
+# Puppet manifest for setting up web servers for web_static deployment
+
+# Update package repositories
+exec { 'apt-update':
+  command => 'sudo apt update -y',
+  path    => '/usr/bin',
 }
--> exec { 'dirs':
-  command => 'mkdir -p /data/web_static/releases/test/ && mkdir -p /data/web_static/shared/',
-  provider => shell,
+
+# Install nginx package
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-update'],
 }
--> exec { 'greetings':
-  command => 'echo "Hello Worldo!" > /data/web_static/releases/test/index.html',
-  provider => shell,
+
+# Define variables and configurations
+$alx_base        = '/data/web_static'
+$alx_tests       = "${alx_base}/releases/test"
+$alx_shared      = "${alx_base}/shared"
+$alx_index       = "${alx_tests}/index.html"
+$alx_current     = "${alx_base}/current"
+$alx_nginx_sites = '/etc/nginx/sites-available/default'
+
+# Create directories and set ownership
+file { [$alx_tests, $alx_shared]:
+  ensure => 'directory',
+} ->
+file { $alx_index:
+  ensure  => 'file',
+  content => 'Hello World!',
+} ->
+file { $alx_current:
+  ensure  => 'link',
+  target  => $alx_tests,
+  force   => true,
+} ->
+exec { 'chown-web-static':
+  command => "sudo chown -R ubuntu:ubuntu ${alx_base}",
+  path    => '/usr/bin',
+  require => File[$alx_tests],
 }
--> exec { 'slink':
-  command => 'ln -sfn /data/web_static/releases/test /data/web_static/current',
-  provider => shell,
+
+# Configure nginx
+$nginx_config = "server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+  add_header X-Served-By $::hostname;
+  root   /var/www/html;
+  index  index.html index.htm;
+
+  location /hbnb_static/ {
+    alias $alx_current;
+  }
+
+  location /redirect_me {
+    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+  }
+
+  error_page 404 /404.html;
+  location /404 {
+    internal;
+    root /var/www/html;
+  }
+}"
+file { $alx_nginx_sites:
+  ensure  => 'file',
+  content => $nginx_config,
+  require => Exec['chown-web-static'],
 }
--> exec { 'dataperms':
-  command => 'chown -R ubuntu:ubuntu /data/',
-  provider => shell,
-}
--> exec { 'defaultsites':
-  command => 'sudo sed -i "s|server_name _;|server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}|" /etc/nginx/sites-enabled/default',
-  provider => shell,
-}
--> exec { 'servicerestart':
-  command => 'sudo service nginx restart',
-  provider => shell,
+# Restart nginx service
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  require   => File[$alx_nginx_sites],
+  subscribe => Exec['chown-web-static'],
 }
