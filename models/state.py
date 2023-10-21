2caacf1ed603186ed27b35cf111cc02403ba80b9
YassineRKB <yassine@arcraven.com>
2023-10-21 18:20:24 +0200

    task 7

--- models/state.py
+++ models/state.py
@@ -21,7 +21,7 @@ class State(BaseModel, Base):
 
             obj_list = []
             strg = storage.all(City)
-            for value in strg:
+            for key, value in strg.items():
                 if self.id == value.state_id:
                     obj_list.append(value)
             return obj_list
