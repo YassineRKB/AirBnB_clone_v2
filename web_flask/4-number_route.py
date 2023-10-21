a583b18321b6d96cdb7e0de3ef1df46cdec615b8
Yassine Rakibi <yassine@arcraven.com>
2023-10-21 16:21:53 +0200

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+HostAddr = "0.0.0.0"
+HostPort = 5000
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """display text"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """display c parameter text"""
+    result = "C {}".format(text.replace('_', ' '))
+    return result
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """display Python parameter text"""
+    result = 'Python {}'.format(text.replace('_', ' '))
+    return result
+
+
+@app.route('/number/<int:n>')
+def text_if_int(n):
+    result = "{:d} is a number".format(n)
+    return result
+
+
+if __name__ == "__main__":
+    app.run(
+        host=HostAddr,
+        port=HostPort,
+        debug=True
+    )
