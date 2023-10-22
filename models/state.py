a83a0386c936fba8478758f537aa9f0f3b14a22d
Yassine Rakibi <yassine@arcraven.com>
2023-10-22 18:27:29 +0200

    task 7

--- models/state.py
+++ models/state.py
@@ -11,7 +11,12 @@ envDB = os.environ.get("HBNB_TYPE_STORAGE")
 class State(BaseModel, Base):
     """State class"""
     __tablename__ = "states"
-    if envDB != "db":
+    if envDB == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            "City", backref="state"
+        )
+    else:
         name = ""
 
         @property
@@ -25,8 +30,3 @@ class State(BaseModel, Base):
                 if self.id == value.state_id:
                     obj_list.append(value)
             return obj_list
-    else:
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            "City", backref="state"
-        )
